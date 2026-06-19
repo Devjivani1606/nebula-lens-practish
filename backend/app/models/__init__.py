94000ba65800a75ea450048e1b23fd5146fbfee7
Manav Merja <manavmerja@gmail.com>
2026-06-20 00:33:43 +0530

    feat(backend): define database schemas and tables

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,12 @@
+from app.models.models import (
+    User,
+    AwsAccount,
+    ScanJob,
+    ServiceScan,
+    Snapshot,
+    Resource,
+    Relationship,
+    SnapshotDiff,
+    NormalizedNode,
+    NormalizedEdge
+)
